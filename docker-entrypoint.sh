507a00671a038b9ff46e2438e72c014e5af574d6
aderseriawan <ade.r.setiawan@gmail.com>
2025-06-04 16:07:15 +0700

    Fix Issue Docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env sh
 set -e
 
-# hanya collectstatic, TANPA migrate
-python manage.py collectstatic --noinput --clear
+python - <<'PY'
+import sys, django, os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NA_Project.settings")
+django.setup(set_prefix=False)
+from django.conf import settings
+if getattr(settings, "STATIC_ROOT", None):
+    import subprocess, shlex
+    cmd = "python manage.py collectstatic --noinput --clear"
+    print("Collecting static files…")
+    subprocess.run(shlex.split(cmd), check=True)
+else:
+    print("STATIC_ROOT not set – skipping collectstatic")
+PY
 
 exec "$@"
